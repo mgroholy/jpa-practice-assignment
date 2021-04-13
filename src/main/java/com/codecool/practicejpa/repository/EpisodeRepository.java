76b1f25109ef6eab906f33b7bcade1d061ecbb5b
Máté Groholy <mate.groholy@gmail.com>
2021-04-13 12:02:37 +0200

    feat: add findEpisodesByDateBetween method in EpisodeRepository.

--- src/main/java/com/codecool/practicejpa/repository/EpisodeRepository.java
+++ src/main/java/com/codecool/practicejpa/repository/EpisodeRepository.java
@@ -2,6 +2,14 @@ package com.codecool.practicejpa.repository;
 
 import com.codecool.practicejpa.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+    @Query("SELECT e FROM Episode e WHERE e.releaseDate BETWEEN :from AND :to")
+    List<Episode> findEpisodesByDateBetween(LocalDate from, LocalDate to);
+
 }
