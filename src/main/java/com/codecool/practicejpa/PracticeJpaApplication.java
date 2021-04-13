4c38444607b3ba5feb17bf47135262c85f53f582
Máté Groholy <mate.groholy@gmail.com>
2021-04-13 09:24:04 +0200

    chore: initialise Spring Boot project.

---
+++ src/main/java/com/codecool/practicejpa/PracticeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.practicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/practicejpa/PracticeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.practicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
